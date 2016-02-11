8ca0ce669f24c47f42de78e35d5ee743c90753f6
Sam Hamm <samhamm@gmail.com>
2016-02-11 12:56:20 -0800

    before class thursday

---
+++ week-1/class-04/README.md
@@ -0,0 +1,22 @@
+## Class 3 (Week 1, Thursday)
+
+* 1:00p: Announcements & Remarks
+  * What to do about Presidents' Day? (Mon 2/15)
+    * Just treat Monday like a normal day
+    * Plan to make up lab and class on Saturday 2/20
+    * Plan to make up lab and class on Sunday 2/21
+    * Shift everything one day later; doing so would move the last day of class to Monday 3/7
+    * Add an extra hour of class to three days (go until 5:00p, in other words) and an extra hour of lab to four days (start at 7:00a on those days)
+  * Quizzes! The first one will be published today
+  * Canvas. Such Canvas.
+  * Tomorrow's lab: we will be pair programming and will start promptly at 8:00a. Your pairs will be decided for you and announced tomorrow morning.
+
+* 1:15p: Code Review including Forking! (Duncan)
+
+* 1:45p: Intro to Functions (Sam)
+
+*2:30p: Break*
+
+* 2:45p: Whiteboard Practice
+
+* 3:00p: Code demo to set up tomorrow's pair programming lab assignment
---
+++ week-1/class-04/demo/app.js
@@ -0,0 +1,25 @@
+// JS file for Feb 11 class demo
+// A guessing game about you with 5 questions for the user, and a different response to the user depending upon if the y got the right or wrong answer.
+
+var one = document.getElementById('one');
+var scottsBeard = document.getElementById('turd-ferguson');
+
+var ans1 = prompt('Did Sam live in South Dakota for 8 years?');
+if (ans1.toLowerCase() === 'n' || ans1.toUpperCase() === 'NO') {
+  // alert('You\'re right! Sam never lived in South Dakota!');
+  one.textContent = 'You\'re right! Sam never lived in South Dakota!';
+} else if (ans1.toLowerCase() === 'y' || ans1.toUpperCase() === 'YES') {
+  // alert('Nope! Sam never lived in South Dakota!');
+  one.textContent = 'Nope! Sam never lived in South Dakota!';
+} else {
+  alert('Please give a yes or no answer.');
+}
+
+var ans2 = prompt('Does Sam have any dogs?');
+if (ans2.toLowerCase() === 'n' || ans2.toUpperCase() === 'NO') {
+  scottsBeard.textContent = 'Wrong! Sam has scottsBeard dogs!';
+} else if (ans2.toLowerCase() === 'y' || ans2.toUpperCase() === 'YES') {
+  scottsBeard.textContent = 'Correct! Demi is a border collie and Parker is a melan collie.';
+} else {
+  alert('Please give a yes or no answer.');
+}
