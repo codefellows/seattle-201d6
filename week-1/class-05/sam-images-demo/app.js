c38b8fad5480895135f21de8f5043de2b03c082c
Sam Hamm <samhamm@gmail.com>
2016-02-15 08:02:36 -0800

    lab assignment

---
+++ week-1/class-05/sam-images-demo/app.js
@@ -0,0 +1,16 @@
+// JS file for Feb 12 class demo
+alert('yo');
+
+// Grab the two elements we need from the DOM
+
+// Take the sentence from the HTML and store it in a variable
+
+// Turn the sentence into an array of the individual words
+
+// Use a for loop to iterate through the array and...
+
+// Log individual words to the console
+
+  // Put the individual words into list elements
+
+  // Add each individual list element to the list in the HTML
