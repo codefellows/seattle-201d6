35199c930335ca04b149deb79deaf9f7ff446d5f
Sam Hamm <samhamm@gmail.com>
2016-02-16 15:53:47 -0800

    end of class

--- week-2/class-07/2-obj-const-demo/demo.js
+++ week-2/class-07/2-obj-const-demo/demo.js
@@ -6,18 +6,42 @@
 // Each object has properties of first name, last initial, hair color, and the class section they are in
 // And, let's give each object a greeting method that utilizes these properties
 
-var noah = {};
-var shelly = {};
+var noah = {
+  firstName: 'Noah',
+  lastInitial: 'X',
+  hairColor: 'blue',
+  classSection: 'seattle-201d6',
+  greeting: function() {
+    alert('My name is ' + this.firstName + ' and my hair is ' + this.hairColor);
+  }
+};
 
-// I would need xxx lines of code to model our class this way
-// (14 students * x lines each)
+// I would need 140 lines of code to model our class this way
+// (14 students * 10 lines each)
 
 // What if I had a function to make these many similar (but unique) objects for me?
 // I can create that by adapting one of my existing objects!
 
+// identical features: classSection, greeting
+// unique features: name, initial, hair
+
+// var shelly = {
+function Student(firstName, lastInitial, middleInitial, hairColor) {
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.middleInitial = middleInitial;
+  this.hairColor = hairColor;
+  this.classSection = 'seattle-201d6';
+  this.greeting = function() {
+    alert('My name is ' + this.firstName + ' and my hair is ' + this.hairColor);
+  };
+};
 
 // Now I can create instances
+var shelly = new Student('Shelly', 'B', 'X', 'light brown');
+var adrienne = new Student('Adrienne', 'GGG', 'Y', 'black');
 
+// shelly.classSection = 'sea-201d1';
 // And add a property to the constructor, that then in inherited by all of the instances, by modifying the prototype
 
 // And also add a method to all instances by modifying the prototype
