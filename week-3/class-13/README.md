1898ec9baca10e5673f8e7a5417e5caa88a48520
Sam Hamm <samhamm@gmail.com>
2016-02-24 19:41:59 -0800

    after class

--- week-3/class-13/README.md
+++ week-3/class-13/README.md
@@ -1,9 +1,10 @@
 ## Class 13 (Week 3, Wednesday)
 
 1. Announcements
-  - Reminder: We're getting closer to Project Week. Prep your pitches! Look at prior projects! PROJECT PITCHES TOMORROW
-  - Schedule is TBA, but we will be watching at least some of the 301 project presentations on Friday afternoon.
-2. Code review (Carrie's code)
+  - Reminder: We're getting closer to Project Week. Prep your pitches! Look at prior projects! PROJECT PITCHES TOMORROW ERMAGHERD
+  - Tomorrow at 4:00p we will have a gues presentation by another instructor candidate, Rick Patci. We hope you can stay through his 30-minute presentation.
+  - 301 final project presentations are on Friday at 10:00a in The Easy. We will go watch them as a class.
+2. Code review
 
 +++BREAK+++
 
--- week-3/class-13/local-storage-demo/app.js
+++ week-3/class-13/local-storage-demo/app.js
@@ -7,8 +7,31 @@ var benton = {
   laughs: function() {
     alert('HAHAHAHAHAHAHAHAHAHA');
   }
-}
+};
 
 var arrayofAwesome = ['Sam', 57, true, 'kittens'];
 
 var arrayOfCrap = ['potato', 12, false, benton, arrayofAwesome];
+
+var clearLS = document.getElementById('clearMahLS');
+
+clearLS.addEventListener('click', function() {
+  console.log('click y\'all');
+  localStorage.clear();
+});
+
+
+// STRINGIFY ALL THE THINGS
+
+// localStorage.setItem()
+
+// STUFF BE IN LOCAL STORAGE
+// OUR STUFF IN LOCAL STORAGE WILL BE IN JSON
+// AND NOT ONLY JSON...
+// ...BUT STRINGIFIED JSON
+
+// localStorage.getItem()
+var retrievedAwesome = localStorage.getItem('awesome');
+
+// UNSTRINGIFY IS ACTUALLY CALLED 'PARSE'
+var parsedAwesome = JSON.parse(retrievedAwesome);
